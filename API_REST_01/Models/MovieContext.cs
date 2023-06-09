e136bd3b50d966a5fef5c7653b205851910756cf
Demetrius Ferreira <demithehomie@gmail.com>
2023-06-08 22:57:58 -0300

    Ainda implementando API REST com C#

---
+++ API_REST_01/Models/MovieContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API_REST_01.Models
+{
+    public class MovieContext : DbContext
+
+
+    {
+
+        public  MovieContext(DbContextOptions<MovieContext> options) : base(options) 
+        { 
+            
+        }
+
+        DbSet<Movie> Movies { get; set; } = null;
+    }
+}
--- API_REST_01/Program.cs
+++ API_REST_01/Program.cs
@@ -1,25 +1,46 @@
-var builder = WebApplication.CreateBuilder(args);
+using API_REST_01.Models;
+using Microsoft.EntityFrameworkCore;
 
-// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+namespace API_REST_01
+{ 
 
-var app = builder.Build();
+    public class Program { 
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        public static void Main(string[] args) 
+        { 
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddDbContext<MovieContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("MovieContext")));
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+
+
+        }
+
+    }
+
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
